1a2ea73f165ff0913fcfc0b48910510ef36849a0
Ethan <ethandhunt@gmail.com>
2022-06-09 10:56:13 +1200

    start

---
+++ imp.lua
@@ -0,0 +1,14 @@
+[[
+Resources:
+https://doi.org/10.2172/5478058 - Packet switching
+
+Packet format:
+
+
+IMP to IMP protocol:
+    Uses flooding because I dont want to do path finding algorithms yet
+    1. Receive message on an outgoing modem
+    2. If 
+]]
+
+
